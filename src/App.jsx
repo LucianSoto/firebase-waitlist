5e56abf294ea2259a88063c4ca1a9f91dfd56f82
LuxianAiolos <luxianaiolos@gmail.com>
2022-10-02 23:19:00 -0700

    Displayed Grid/ListItem in List Component
    Although Tailwind does not allow you to customize grid template columns ;(

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ import List from './components/List'
 function App() {
   return (
     <div id="app" className="bg-purple-100 flex flex-col items-center justify-center">
+      <header className=" mb-4 rounded-t-md">
+        <h1 className='text-4xl font-bold mt-4'>Firebase Waitlist</h1>
+      </header>
+      {/* Join Button */}
       {/* need to add more componenets 
           such as ppl waiting time estimate etc in order to make use of redux  
       */}
--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
+import { dummyData } from './dummyListData'
+import { FaCheck } from 'react-icons/fa'
+
 
 const List = () => {
+
+  const listData = dummyData.map((item, i) => {
+    return (
+    <div style={{display: 'grid', gridTemplateColumns: '4fr 1fr 1fr 1fr' }} key={i}>
+      <p className="name">{item.name}</p>
+      {/* <p className="phone">{item.phone}</p> */}
+      <p className="size">{item.partySize}</p>
+      <p className="of-age">{item.ofAge ? 'Bar' : 'Main'}</p>
+      <FaCheck/>
+    </div>
+    )
+  })
+
+  console.log(listData, 'listData')
   return (
-    <div className='flex flex-col bg-gray-100 h-5/6 w-5/6 rounded-md'>
-      <header className="bg-pink-200 rounded-t-md">
-        <h1 className='text-4xl font-bold mt-4'>Firebase Waitlist</h1>
-      </header>
+    <div className='flex flex-col bg-gray-100 h-5/6 w-5/6 rounded-md shadow-lg'>
+      {listData}
     </div>
   )
 }
