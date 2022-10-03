0c642f17a1e1965ef531d2a83d858715515db0d0
LuxianAiolos <luxianaiolos@gmail.com>
2022-10-02 22:26:37 -0700

    Update dummyListData.js

--- src/components/dummyListData.js
+++ src/components/dummyListData.js
@@ -1,5 +1,26 @@
 export const dummyData = [
   {
-    name: 'John Sno'
-  }
+    name: 'John Sno',
+    phone: 2068789876,
+    partySize: 3,
+    ofAge: true,
+  },
+  {
+    name: 'Eddard Stark',
+    phone: 2068789999,
+    partySize: 6,
+    ofAge: false,
+  },
+  {
+    name: 'Bran Stark',
+    phone: 2068788888,
+    partySize: 5,
+    ofAge: false,
+  },
+  {
+    name: 'Tyrion Lannister',
+    phone: 6195648764,
+    partySize: 8,
+    ofAge: true,
+  },
 ]
