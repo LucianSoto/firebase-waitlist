96bd127c8e49079813809ac4b8e971ee5cf2a758
LuxianAiolos <luxianaiolos@gmail.com>
2022-10-04 12:21:52 -0700

    fixed some css for list item

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -7,8 +7,8 @@ const List = () => {
 
   const listData = dummyData.map((item, i) => {
     return (
-    <div className='grid grid-cols-[4fr_1fr_1fr_1fr]' key={i}>
-      <p className="name">{item.name}</p>
+    <div className='grid grid-cols-[2fr_1fr_1fr_.5fr] items-center border-b-2 my-4 w-11/12 pb-2' key={i}>
+      <p className="name pl-4">{item.name}</p>
       {/* <p className="phone">{item.phone}</p> */}
       <p className="size">{item.partySize}</p>
       <p className="of-age">{item.ofAge ? 'Bar' : 'Main'}</p>
@@ -19,7 +19,7 @@ const List = () => {
 
   console.log(listData, 'listData')
   return (
-    <div className='flex flex-col bg-gray-100 h-5/6 w-5/6 rounded-md shadow-lg'>
+    <div className='flex flex-col items-center bg-gray-100 h-5/6 w-5/6 rounded-md shadow-lg p-4'>
       {listData}
     </div>
   )
