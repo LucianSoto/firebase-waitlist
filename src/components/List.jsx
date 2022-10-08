07c8ec6d8e7f5d978b197276c5a3dd7c21843a9a
LuxianAiolos <luxianaiolos@gmail.com>
2022-10-08 08:52:43 -0700

    Firebase data retrieval success

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -17,33 +17,38 @@ const List = () => {
     // const res = await getDocs(collection(db, 'waitlist-1'))
     //   console.log(res.docs)
     // const data = res.data
-    const q = query(collection(db, 'waitlist-1'), where("ofAge", "==", true))
+    const q = query(collection(db, 'waitlist-1'))
 
-    const querySnapshot = await getDocs(q)
+    const querySnap = await getDocs(q)
 
-    console.log(querySnapshot.docs[0]._document, 'querysmap')
-    querySnapshot.forEach((doc) => {
-      console.log(doc.data)
+    let listArr = []
+
+    querySnap.forEach((doc) => {
+      listArr.push(
+        doc.data()
+      )
     })
     
-
+    setList(listArr)
   }
 
-  // const listData = dummyData.map((item, i) => {
-  //   return (
-  //   <div className='grid grid-cols-[2fr_1fr_1fr_.5fr] items-center border-b-2 my-4 w-11/12 pb-2' key={i}>
-  //     <p className="name pl-4">{item.name}</p>
-  //     {/* <p className="phone">{item.phone}</p> */}
-  //     <p className="size">{item.partySize}</p>
-  //     <p className="of-age">{item.ofAge ? 'Bar' : 'Main'}</p>
-  //     <FaCheck/>
-  //   </div>
-  //   )
-  // })
+  console.log(list)
+
+  const listData = list.map((item, i) => {
+    return (
+    <div className='grid grid-cols-[2fr_1fr_1fr_.5fr] items-center border-b-2 my-4 w-11/12 pb-2' key={i}>
+      <p className="name pl-4">{item.name}</p>
+      {/* <p className="phone">{item.phone}</p> */}
+      <p className="size">{item.partySize}</p>
+      <p className="of-age">{item.ofAge ? 'Bar' : 'Main'}</p>
+      <FaCheck/>
+    </div>
+    )
+  })
 
   return (
     <div className='flex flex-col items-center bg-gray-100 h-5/6 w-5/6 rounded-md shadow-lg p-4'>
-      {/* {listData} */}
+      {listData}
     </div>
   )
 }
