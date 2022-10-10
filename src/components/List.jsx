85b1a240335fb92d245e87116093db6c29a3e194
LuxianAiolos <luxianaiolos@gmail.com>
2022-10-10 01:01:28 -0700

    FB Waitlist data retrieval sucess

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 // import { dummyData } from './dummyListData'
 import { FaCheck } from 'react-icons/fa'
 import { collection, getDocs, doc, query, where} from 'firebase/firestore'
@@ -7,48 +7,56 @@ import { db } from '../firebase.config'
 const List = () => {
   const [isLoading, setIsLoading] = useState(true)
   const [list, setList] = useState()
+  const mounted = useRef(false)
   
   useEffect(()=> {
+    // if(mounted.current === false) {
+    //   console.log('hi')
+    //   fetchList() 
+    //   mounted.current = true
+    // }
     console.log('hi')
-    fetchList() 
+    const fetchList = async () => {
+        const q = query(collection(db, 'waitlist-1'))
+    
+        const querySnap = await getDocs(q)
+    
+        let listArr = []
+    
+        querySnap.forEach((doc) => {
+          return listArr.push(
+            doc.data()
+          )
+        })
+        
+        setList(listArr)
+        setIsLoading(false)
+    }
+    fetchList()
   },[])
   
-  const fetchList = async () => {
-    // const res = await getDocs(collection(db, 'waitlist-1'))
-    //   console.log(res.docs)
-    // const data = res.data
-    const q = query(collection(db, 'waitlist-1'))
-
-    const querySnap = await getDocs(q)
-
-    let listArr = []
-
-    querySnap.forEach((doc) => {
-      listArr.push(
-        doc.data()
-      )
-    })
-    
-    setList(listArr)
-  }
 
   console.log(list)
+  // console.log(mounted.current)
 
-  const listData = list.map((item, i) => {
-    return (
-    <div className='grid grid-cols-[2fr_1fr_1fr_.5fr] items-center border-b-2 my-4 w-11/12 pb-2' key={i}>
-      <p className="name pl-4">{item.name}</p>
-      {/* <p className="phone">{item.phone}</p> */}
-      <p className="size">{item.partySize}</p>
-      <p className="of-age">{item.ofAge ? 'Bar' : 'Main'}</p>
-      <FaCheck/>
-    </div>
-    )
-  })
-
+  
   return (
     <div className='flex flex-col items-center bg-gray-100 h-5/6 w-5/6 rounded-md shadow-lg p-4'>
-      {listData}
+      {list ? 
+      list.map((item, i) => {
+        return (
+         <div className='grid grid-cols-[2fr_1fr_1fr_.5fr] items-center border-b-2 my-4 w-11/12 pb-2' key={i}>
+           <p className="name pl-4">{item.name}</p>
+           {/* <p className="phone">{item.phone}</p> */}
+           <p className="size">{item.size}</p>
+           <p className="of-age">{item.ofAge ? 'Bar' : 'Main'}</p>
+           <FaCheck/>
+         </div>
+         )
+       }) 
+       :
+       "laoding..."
+      }
     </div>
   )
 }
